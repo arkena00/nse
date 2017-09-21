f69672277addbcb78bdb3579d8db2ba763b4a93b
Clément François <clement.francois@epitech.eu>
2017-09-21 16:24:08 +0200

    Add detail nested structure inside table

--- include/ndb/table.hpp
+++ include/ndb/table.hpp
@@ -3,18 +3,8 @@
 
 namespace ndb
 {
-    class table_base {};
-
-    template<typename D>
-    class table : table_base
+    class table
     {
-        template<typename Derived>
-        struct detail_t {
-            constexpr detail_t() { name = Derived::name_; };
-            const char *name{};
-        };
-    public:
-        struct detail_t<D> detail{};
     };
 } // ndb
 
