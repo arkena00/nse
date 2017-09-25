ca101f3022546812019d2d9be940762021d499e3
ads00 <adess00@hotmail.fr>
2017-09-25 16:15:20 +0200

    refactor

--- include/ndb/field.hpp
+++ include/ndb/field.hpp
@@ -23,20 +23,17 @@ namespace ndb
         template<size_t Size>
         struct size
         {
-            static constexpr auto size_ = Size;
+            static constexpr auto value = Size;
         };
     };
 
-    namespace detail
+    // default field value : sizeof(T) for static_field, 0 for field_entity
+    template<class T>
+    constexpr size_t default_size()
     {
-        // default field value : sizeof(T) for static_field, 0 for field_entity
-        template<class T>
-        constexpr size_t default_size()
-        {
-            if constexpr (ndb::is_table<T>) return 0;
-            else return sizeof(T);
-        }
-    } // detail
+        if constexpr (ndb::is_table<T>) return 0;
+        else return sizeof(T);
+    }
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////          COMMON_FIELD          ////////////////////////
@@ -47,11 +44,11 @@ namespace ndb
     public:
         struct detail
         {
-            static constexpr bool is_primary = option_trait::has<field_option::primary, Option>::value;
-            static constexpr bool is_auto_increment = option_trait::has<field_option::autoincrement, Option>::value;
-            static constexpr bool is_unique = option_trait::has<field_option::unique, Option>::value;
-            static constexpr bool is_not_null = option_trait::has<field_option::not_null, Option>::value;
-            static constexpr size_t size = Size::size_;
+            static constexpr bool is_primary = has_option<field_option::primary, Option>::value;
+            static constexpr bool is_auto_increment = has_option<field_option::autoincrement, Option>::value;
+            static constexpr bool is_unique = has_option<field_option::unique, Option>::value;
+            static constexpr bool is_not_null = has_option<field_option::not_null, Option>::value;
+            static constexpr size_t size = Size::value;
         };
 
         using Detail_ = detail;
@@ -61,16 +58,17 @@ namespace ndb
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////             FIELD              ////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-    template<class T, class = void, class...> class field;
+    template<class T, class = void, class...>
+    class field;
 
     // field<T>
     template<class T>
     class field<T, typename std::enable_if_t<!ndb::is_table<T>>> :
-        public common_field<field_base::size<detail::default_size<T>()>, option<field_option::none>>, field_base
+        public common_field<field_base::size<default_size<T>()>, ndb::option<>>, field_base
     {
     public:
         using type = T;
-        using typename common_field<field_base::size<detail::default_size<T>()>, option<field_option::none>>::Detail_;
+        using typename common_field<field_base::size<default_size<T>()>, ndb::option<>>::Detail_;
     };
 
     // field<T, Size>
@@ -86,11 +84,11 @@ namespace ndb
     // field<T, option>
     template<class T, class... Option_code>
     class field<T, option<Option_code...>> :
-        public common_field<field_base::size<detail::default_size<T>()>, option<Option_code...>>, field_base
+        public common_field<field_base::size<default_size<T>()>, option<Option_code...>>, field_base
     {
     public:
         using type = T;
-        using typename common_field<field_base::size<detail::default_size<T>()>, option<Option_code...>>::Detail_;
+        using typename common_field<field_base::size<default_size<T>()>, option<Option_code...>>::Detail_;
     };
 
     // field<T, Size, Option>
@@ -116,11 +114,11 @@ namespace ndb
     // field<table>
     template<class T>
     class field<T, typename std::enable_if_t<ndb::is_table<T>>> :
-        public common_field<field_base::size<detail::default_size<T>()>, option<field_option::none>>, field_base
+        public common_field<field_base::size<default_size<T>()>, option<field_option::none>>, field_base
     {
     public:
         using type = T;
-        using typename common_field<field_base::size<detail::default_size<T>()>, option<field_option::none>>::Detail_;
+        using typename common_field<field_base::size<default_size<T>()>, option<field_option::none>>::Detail_;
     };
 } // ndb
 
--- include/ndb/option.hpp
+++ include/ndb/option.hpp
@@ -1,46 +1,32 @@
 #ifndef OPTION_H_NDB
 #define OPTION_H_NDB
 
-/*
- * Option fields
- */
-namespace ndb {
-    namespace field_option {
+namespace ndb
+{
+    struct field_option
+    {
         struct none { static constexpr auto value = 0; };
         struct unique { static constexpr auto value = 1; };
         struct autoincrement { static constexpr auto value = 2; };
         struct primary { static constexpr auto value = 4; };
         struct not_null { static constexpr auto value = 8; };
-    }
-}
+    };
 
-/*
- * Option traits
- */
-namespace option_trait
-{
+    template<class...>
+    class option {};
+
     template<class Needle, class Haystack>
-    class has;
+    class has_option;
 
     template<class Needle, template<class...> class Haystack, class T1, class... T>
-    struct has<Needle, Haystack<T1, T...>> { static constexpr bool value = has<Needle, Haystack<T...>>::value; };
+    struct has_option<Needle, Haystack<T1, T...>> { static constexpr bool value = has_option<Needle, Haystack<T...>>::value; };
 
     template<class Needle, template<class...> class Haystack>
-    struct has<Needle, Haystack<>> : std::false_type {};
+    struct has_option<Needle, Haystack<>> : std::false_type {};
 
     template<class Needle, template<class...> class Haystack, class... T>
-    struct has<Needle, Haystack<Needle, T...>> : std::true_type {};
-}
+    struct has_option<Needle, Haystack<Needle, T...>> : std::true_type {};
 
-/*
- * Option structure
- */
-namespace ndb
-{
-    template<class...>
-    class option
-    {
-    };
 } // ndb
 
 #endif // OPTION_H_NDB
